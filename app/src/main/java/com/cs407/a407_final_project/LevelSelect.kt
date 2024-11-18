058bb3cd6397134062f6ea5e0be878826c4475da
Shawn Feng <shawnfeng3954@gmail.com>
2024-11-18 16:25:59 -0600

    shawn: start setting up level select screen

--- app/src/main/java/com/cs407/a407_final_project/LevelSelect.kt
+++ app/src/main/java/com/cs407/a407_final_project/LevelSelect.kt
@@ -1,28 +1,20 @@
 package com.cs407.a407_final_project
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LevelSelect : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_level_select)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        findViewById<Button>(R.id.get_started).setOnClickListener {
-            val intent = Intent(this, LevelSelect::class.java)
-            startActivity(intent)
-        }
-
     }
 }
